8f42d6e48bb04b3fdf4bfa73c3aa85760fda7e6a
OkayegTeatime <yusifrazz@gmail.com>
2023-09-14 07:30:57 -0600

    sh script

---
+++ pull_and_run.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+git pull
+while [[ $# -gt 0 ]]; do 
+    case "$1" in 
+        -r|--rebuild)
+            echo "Rebuilding from scratch..."
+            rm -rf build            
+            shift 1
+            ;;
+        *)
+            echo "Unknown arg '$1'"
+            exit 1
+            ;;
+    esac
+done
+
+export LD_LIBRARY_PATH=/home/yusif/AMP-Tools-public/install/ubuntu22/lib:$LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
+mkdir -p build && cd build
+
+architecture=$(uname -m)
+cmake ./.. -DAMP_BUILD_LIB=OFF -DSYSTEM_ARCHITECTURE="${architecture}" 
+
+make
+if [ $? == 0 ]; then
+    clear
+    cd bin
+    ./main 
+else
+    echo "Build Failed!"
+fi

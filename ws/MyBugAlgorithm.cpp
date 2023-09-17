4c4ae950b7c0ff2fca5b3d024601bc86196892ae
Nicolas Perrault <nicolasperrault@Nicolass-MBP.hsd1.co.comcast.net>
2023-09-17 15:25:09 -0600

    both implementations done

--- ws/MyBugAlgorithm.cpp
+++ ws/MyBugAlgorithm.cpp
@@ -16,16 +16,25 @@ amp::Path2D MyBugAlgorithm::plan(const amp::Problem2D& problem) {
     float stepSize = 0.1;
     float delta = 0.1;
 
-    //step 1: initialize bug
-    Bug2 bug2 = Bug2(problem, stepSize, delta);
-
+    // Bug 1 implementation:
+    Bug1 bug1 = Bug1(problem, stepSize, delta);
     path.waypoints.push_back(problem.q_init);
-    while(bug2.position != problem.q_goal){
-        bug2.step();
-        bug2.waypoints.push_back(bug2.position);
-        path.waypoints.push_back(bug2.position);
+    while(bug1.position != problem.q_goal){
+        bug1.step();
+        bug1.waypoints.push_back(bug1.position);
+        path.waypoints.push_back(bug1.position);
     }
     return path;
+    
+    //Bug 2 implementation:
+    // Bug2 bug2 = Bug2(problem, stepSize, delta);
+    // path.waypoints.push_back(problem.q_init);
+    // while(bug2.position != problem.q_goal){
+    //     bug2.step();
+    //     bug2.waypoints.push_back(bug2.position);
+    //     path.waypoints.push_back(bug2.position);
+    // }
+    // return path;
 
 }
 

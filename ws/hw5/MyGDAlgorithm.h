647d425866edc02c73751cf5ffabac03dd87d065
collin ubuntu <collinhudson.app@gmail.com>
2023-10-09 18:14:53 -0600

    notes from office hours

--- ws/hw5/MyGDAlgorithm.h
+++ ws/hw5/MyGDAlgorithm.h
@@ -10,6 +10,22 @@ class MyGDAlgorithm : public amp::GDAlgorithm {
         
         // Override and implement the bug algorithm in the plan method. The methods are declared here in the `.h` file
         virtual amp::Path2D plan(const amp::Problem2D& problem) override;
+
+        // Split up workspace into regions in front of vertices and in front of edges.
+        /*
+              v     edge
+                 __________ vertex region     
+               /           |
+         edge /            | edge region
+             /             |
+            |______________|
+           v      edge      v 
+        */
+        // Allows you to check distance by either using just the vertex or have to use normal vecs to edge
+
+        // Don't need to create vector field plots
+
+        //benchmarks only really worth extra credit 
         
     private:
         // Add any member variables here...

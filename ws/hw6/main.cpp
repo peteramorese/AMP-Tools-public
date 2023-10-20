48ade437c5aa2ba81bca04d7a39c2e840a1dcfd2
collin ubuntu <collinhudson.app@gmail.com>
2023-10-19 18:04:26 -0600

    arm :))))

--- ws/hw6/main.cpp
+++ ws/hw6/main.cpp
@@ -11,16 +11,28 @@ using namespace amp;
 int main(int argc, char** argv) {
     amp::RNG::seed(amp::RNG::randiUnbounded());
     // MyManipWFAlgo mani;
+    LOG("Problem 1");
+
+
+
+    LOG("Problem 2");
     MyGridCSpace2DConstructor cons;
-    cons.getGridWidth() = 0.1;
+    cons.getGridWidth() = 0.05;
     MyManipWFAlgo mani(cons);
     MyLinkManipulator mani3;
     const amp::Problem2D env3 = HW6::getHW4Problem3();
     auto path = mani.plan(mani3,env3);
     MyGridCSpace2D plotEnv3(std::ceil((cons.getX0_bounds().second - cons.getX0_bounds().first)/cons.getGridWidth()),std::ceil((cons.getX1_bounds().second - cons.getX1_bounds().first)/cons.getGridWidth()),cons.getX0_bounds().first,cons.getX0_bounds().second,cons.getX1_bounds().first,cons.getX1_bounds().second);
-     Visualizer::makeFigure(env3);
     Visualizer::makeFigure(plotEnv3.makeCSpace(mani3,env3),path);
+    Eigen::Vector2d lowerB(cons.getX0_bounds().first,cons.getX1_bounds().first);
+    Eigen::Vector2d upperB(cons.getX0_bounds().second,cons.getX1_bounds().second);
+    unwrapPath(path,lowerB,upperB);
+    Visualizer::makeFigure(env3,mani3,path);
+
+
+
+
     Visualizer::showFigures();
-    // amp::HW6::grade<MyPointWFAlgo, MyManipWFAlgo, MyAStarAlgo>("nonhuman.biologic@myspace.edu", argc, argv, std::make_tuple(), std::make_tuple("hey therre"), std::make_tuple());
+    // amp::HW6::grade<MyPointWFAlgo, MyManipWFAlgo, MyAStarAlgo>("collin.hudson@colorado.edu", argc, argv, std::make_tuple(), std::make_tuple("hey therre"), std::make_tuple());
     return 0;
 }

b5a9e0282be03d31c5c9691fdd8ea7114d439c6e
collin ubuntu <collinhudson.app@gmail.com>
2023-10-24 16:23:58 -0600

    still fix yo stuff man

---
+++ ws/hw7/MyPointMotionPlanner.h
@@ -0,0 +1,25 @@
+// #pragma once
+
+// #include "AMPCore.h"
+// #include "hw/HW7.h"
+// #include <Eigen/LU>
+
+
+
+// /// @brief Derive this class and implement your algorithm in the `plan` method. 
+// class MyPRM : public HW7::PointMotionPlanner2D {
+//     public:
+//         /// @brief Solve a motion planning problem. Create a derived class and override this method
+//         //virtual amp::Path2D plan(const amp::Problem2D& problem) = 0;
+
+//         virtual ~PRM() {}
+// };
+
+// /// @brief Derive this class and implement your algorithm in the `plan` method. 
+// class MyGoalBiasRRT : public HW7::PointMotionPlanner2D {
+//     public:
+//         /// @brief Solve a motion planning problem. Create a derived class and override this method
+//         //virtual amp::Path2D plan(const amp::Problem2D& problem) = 0;
+
+//         virtual ~GoalBiasRRT() {}
+// };
---
+++ ws/hw7/main.cpp
@@ -0,0 +1,15 @@
+#include "AMPCore.h"
+// #include "hw/HW7.h"
+#include "MyLinkManipulator.h"
+#include "MyConfigurationSpace.h"
+#include "MyPointMotionPlanner.h"
+
+using namespace amp;
+
+int main(int argc, char** argv) {
+    amp::RNG::seed(amp::RNG::randiUnbounded());
+
+    // Visualizer::showFigures();
+    // amp::HW6::grade(wf1g, wf2g, Astar, "collin.hudson@colorado.edu", argc, argv);
+    return 0;
+}

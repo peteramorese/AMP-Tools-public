e36d2e4f6cbd311fc78c4914c5cb340be7b88cfb
Yusif <yusifrazz@gmail.com>
2023-10-27 16:54:41 -0600

    RRT Working

--- ws/hw7/MyPRM.cpp
+++ ws/hw7/MyPRM.cpp
@@ -64,3 +64,81 @@ void MyPRM::connectNieghbors(const vector<amp::Obstacle2D> obstacles) {
         }
     }
 }
+
+amp::Path2D MyRRT::plan(const amp::Problem2D& problem) {
+    Path2D path;
+    limits.push_back({problem.x_min, problem.x_max});
+    limits.push_back({problem.y_min, problem.y_max});
+    VectorXd qRand, nearest;
+    points[0] = problem.q_init;
+    std::map<int, int> parents;
+    int ind = 1;
+    bool success = false;
+    while (points.size() < n) {
+        qRand = getRandomPoint();
+        // if (!isPointInCollision(qRand, problem.obstacles)) {
+            // path.waypoints.push_back(qRand);
+            pair<int, VectorXd> nearest = findNearest(qRand, problem.obstacles);
+            if (nearest.first != -1) {
+                graphPtr->connect(nearest.first, ind, (points[nearest.first] - nearest.second).norm());
+                points[ind] = nearest.second;
+                parents[ind] = nearest.first;
+                // cout << "Adding point: ( " << nearest.second(0) << ", " << nearest.second(1) << ")\n"; 
+                ind++;
+                if ((nearest.second - problem.q_goal).norm() < 1) {
+                    cout << "Goal found\n";
+                    success = true;
+                    break;
+                }
+            }
+        // }
+    }
+    ind--;
+    int node = ind;
+    while (node != 0) {
+        // cout << "Adding node " << node << " at ( " << points[node](0) << ", " << points[node](1) << ")\n"; 
+        path.waypoints.insert(path.waypoints.begin(), points[node]);
+        node = parents[node];
+    }
+    path.waypoints.insert(path.waypoints.begin(), problem.q_init);
+    if (success) path.waypoints.push_back(problem.q_goal);
+    return path;
+}
+
+pair<int, VectorXd> MyRRT::findNearest(const VectorXd& point, const vector<amp::Obstacle2D> obstacles) {
+    VectorXd nearest = points[0];
+    int ind = 0;
+    for (int i = 1; i < points.size(); i++) {
+        if ((point - points[i]).norm() < (point - nearest).norm()) {
+            nearest = points[i];
+            ind = i;
+        }
+    }
+    int steps = 10;
+    VectorXd step = (point - nearest)/steps;
+    VectorXd endpoint = nearest;
+    for (int i = 0; i < steps; i++) {
+        endpoint += step;
+        if (isLineInCollision(nearest, endpoint, obstacles)) { 
+            endpoint -= step;
+            if (i == 0) ind = -1;
+            break;
+        }
+    }
+    return {ind, endpoint};
+}
+
+VectorXd MyRRT::getRandomPoint() {
+    int dim = 2;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::pair<double, double> limit;
+    VectorXd randomPoint(dim);
+    for (int i = 0; i < dim; i++) {
+        limit = limits[i];
+        std::uniform_real_distribution<double> dist(limit.first, limit.second);
+        randomPoint(i) = dist(gen);
+    }
+    return randomPoint;
+}
+
--- ws/hw7/MyPRM.h
+++ ws/hw7/MyPRM.h
@@ -35,3 +35,19 @@ class MyPRM : public amp::PRM2D {
         // Eigen::VectorXd init, goal;
 };
 
+class MyRRT : public amp::GoalBiasRRT2D {
+    public:
+        MyRRT(int n, double r)
+        : n(n), r(r) { graphPtr = std::make_shared<amp::Graph<double>>();}
+        virtual amp::Path2D plan(const amp::Problem2D& problem) override; 
+        Eigen::VectorXd getRandomPoint();
+        void connectNieghbors(const vector<amp::Obstacle2D> obstacles);
+        pair<int, Eigen::VectorXd> findNearest(const Eigen::VectorXd& point, const vector<amp::Obstacle2D> obstacles);
+    private:
+        int n;
+        double r;
+        std::map<int, Vector2d> points;
+        vector<std::pair<double, double>> limits;
+        std::shared_ptr<amp::Graph<double>> graphPtr;
+        // Eigen::VectorXd init, goal;
+};

c01e25fb2f7cb82f70282201b31029432f86585e
collin ubuntu <collinhudson.app@gmail.com>
2023-12-11 14:37:30 -0700

    planning method being built

--- ws/FinalProject/MyFlightPlanner.cpp
+++ ws/FinalProject/MyFlightPlanner.cpp
@@ -9,14 +9,11 @@ amp::MultiAgentPath2D MyFlightPlanner::plan(const UASProblem& problem){
     RRT.getN() = 50000;
     RRT.getS() = 0.5;
     RRT.getG() = 0.15;
-    checkPath c;
-
-    const auto noLOS = std::mem_fn(&checkPath::lineCollision2D);
+    FlightChecker c;
 
     auto start = std::chrono::high_resolution_clock::now();
 
     // initialize LOS graph with empty edge sets for each Ground Agent and UAS
-    std::vector<std::set<int>> losGraph;
     for(int j = 0; j < (problem.numGA() + problem.numAgents()); j++){
         std::set<int> temp;
         for(int k = 0; k < (problem.numGA() + problem.numAgents()); k++){
@@ -34,30 +31,99 @@ amp::MultiAgentPath2D MyFlightPlanner::plan(const UASProblem& problem){
             else{
                 posk = problem.agent_properties[k].q_init;
             }
-            if(!noLOS(c,posj,posk,problem)){
+            if(c.inLOS(posj,posk,problem)){
                 temp.insert(k);
             }
         }
-        losGraph.push_back(temp);
+        c.losGraph.push_back(temp);
     }
 
-    amp::MultiAgentPath2D soln = RRT.plan(problem);
-
-
-
-
-
-
-
-
-
-
+    //{
+        amp::MultiAgentPath2D path;
+        std::vector<sampleS> samples;
+        // Construct init and goal super states
+        Eigen::VectorXd init(2*problem.numAgents());
+        for(int j = 0; j < 2*problem.numAgents(); j += 2){
+            init(j) = problem.agent_properties[j/2].q_init(0);
+            init(j + 1) = problem.agent_properties[j/2].q_init(1);
+        }
+        // LOG(init);
+        samples.push_back(sampleS(init,-1));
+        Eigen::VectorXd q_rand(3*problem.numAgents());
+        bool soln = false;
+        int minID = 0;
+        double tempMin = 0;
+        int tries = 0;
+        int numNodes = 1;
+        while(!soln && tries < getN()){
+            //Generate q_rand
+            int tst = amp::RNG::randi(0,100);
+            for(int j = 0; j < 3*problem.numAgents(); j += 3){
+                q_rand(j) = amp::RNG::randd(problem.x_min, problem.x_max); //x
+                q_rand(j+1) = amp::RNG::randd(problem.y_min, problem.y_max); //y
+                q_rand(j+2) = amp::RNG::randd(0, 2*M_PI); //heading angle (velocity i.e. groundspeed is constant)
+            }
+            //Find closest node in tree to q_rand
+            minID = 0;
+            tempMin = (samples[0].xy - q_rand).norm();
+            for(int k = 1; k < samples.size(); k++){
+                if((samples[k].xy - q_rand).norm() < tempMin){
+                    tempMin = (samples[k].xy - q_rand).norm();
+                    minID = k;
+                }
+            }
+            splitAndStep2D(samples[minID].xy, q_rand, getS());
+            // q_rand = ((1 - stepSize/tempMin)*samples[minID].xy + (stepSize/tempMin)*q_rand);
+            if(!c.multDiskCollision2D(samples[minID].xy, q_rand, problem)){
+                // LOG("Adding state: " << q_rand);
+                numNodes++;
+                sampleS q_randS(q_rand,minID);
+                samples.push_back(q_randS);
+                if(numNodes >= problem.maxTime){
+                    soln = true;
+                }
+            }
+            tries++;
+        }
+        std::list<Eigen::VectorXd> l;
+        if(soln){
+            sampleS testS = samples.back();
+            while(testS.back != -1){
+                l.push_front(testS.xy);
+                // LOG("Pushing to front state " << testS.xy);
+                testS = samples[testS.back];
+            }
+            l.push_front(init);
+            for(int j = 0; j < 2*problem.numAgents(); j += 2){
+                amp::Path2D tempPath;
+                for (std::list<Eigen::VectorXd>::iterator it=l.begin(); it != l.end(); ++it){
+                    Eigen::VectorXd tempListEle = *it;
+                    Eigen::Vector2d tempVec(tempListEle(j),tempListEle(j+1));
+                    // LOG("Adding tempVec " << tempVec);
+                    // LOG("From tempListEle " << tempListEle);
+                    tempPath.waypoints.push_back(tempVec);
+                }
+                // LOG("Adding tempPath");
+                path.agent_paths.push_back(tempPath);
+                
+            }
+        }
+        else{
+            if(tries == getN()){
+                LOG("Ran outta tries buddy :(");
+            }else{
+                LOG("Couldn't find path :(");
+            }
+        }
+        getN() = numNodes;
+        
+    //}
 
 
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = duration_cast<std::chrono::milliseconds>(stop - start);
-    time = duration.count();
-    numIterations = RRT.getN();
-    return soln;
+    getT() = duration.count();
+    return path;
 }
+
 
--- ws/FinalProject/MyFlightPlanner.h
+++ ws/FinalProject/MyFlightPlanner.h
@@ -12,21 +12,66 @@ struct UASProblem : public amp::MultiAgentProblem2D {
 
     //Function for GA positions given time
 
-    std::vector<Eigen::Vector2d> initGA; //vector of initial ground agent positions
+    const std::vector<Eigen::Vector2d> initGA; //vector of initial ground agent positions
+    const std::vector<Eigen::Vector2d> finalGA; //vector of final ground agent positions
+    const std::vector<int> endGAt; //vector of times (number of steps) for each GA to reach finalGA
+    const int maxTime; //Largest element in endGAt
     inline int numGA() const {return initGA.size();};
+    //Function that returns position of a GA at time t (straight line)
+    Eigen::Vector2d GApos(int time, int idxGA) const {
+        if(time <= endGAt[idxGA]){
+            return initGA[idxGA]*(1 - time/endGAt[idxGA]) + finalGA[idxGA]*(time/endGAt[idxGA]);
+        }
+        else{
+            return finalGA[idxGA];
+        }
+    };
 };
 
-class MyFlightPlanner{
+class MyFlightPlanner : public MyGoalBiasRRTND{
     public:
         /// @brief Solve a motion planning problem. Derive class and override this method
         /// @param problem Multi-agent motion planning problem
         /// @return Array of paths that are ordered corresponding to the `agent_properties` field in `problem`.
         amp::MultiAgentPath2D plan(const UASProblem& problem);
+};
 
-    int& getT(){return time;};
-    int& getN(){return numIterations;};
-    private:
-        int time = 0;
-        int numIterations = 20000;
+class FlightChecker : public checkPath{
+    public:
+        bool inLOS(const Eigen::Vector2d state0, const Eigen::Vector2d state1, const amp::Environment2D& obs){
+            return !lineCollision2D(state0, state1, obs);
+        }
+
+        void updateLOS( Eigen::VectorXd state, const UASProblem& problem, int time){
+
+            for(int j = 0; j < (problem.numGA() + problem.numAgents()); j++){
+                std::set<int> temp;
+                for(int k = 0; k < (problem.numGA() + problem.numAgents()); k++){
+                    Eigen::Vector2d posj;
+                    Eigen::Vector2d posk;
+                    if(j < problem.numGA()){
+                        posj = problem.initGA[j];
+                    }
+                    else{
+                        posj = problem.agent_properties[j].q_init;
+                    }
+                    if(k < problem.numGA()){
+                        posk = problem.initGA[k];
+                    }
+                    else{
+                        posk = problem.agent_properties[k].q_init;
+                    }
+                    if(inLOS(posj,posk,problem)){
+                        temp.insert(k);
+                    }
+                }
+                losGraph.push_back(temp);
+            }
+
+
+        }
+
+        std::vector<std::set<int>> losGraph;
+
 };
 

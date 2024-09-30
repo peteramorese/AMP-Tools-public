6ef5bd09a2d6ac9d663fdd663b99798801b04cc6
Yusif <yusifrazz@gmail.com>
2024-09-29 19:52:34 -0600

    HW5 starter code added

--- scripts/VisualizeEnvironment.py
+++ scripts/VisualizeEnvironment.py
@@ -10,8 +10,8 @@ import sys
 visualize_config = {
     "path_line_width": 2.0,
     "path_line_style": '--',
-    "path_line_color": "blue",
-    "path_point_marker": "o",
+    "path_line_color": "darkcyan",
+    "path_point_marker": ".",
     "show_grid": True,
     "text_font_size": 10.0,
     "show_text": False,
@@ -19,7 +19,7 @@ visualize_config = {
     "arrow_color": "path_line_color",
     "arrow_scale": 20.0,
     "arrow_head_width": 2,
-    "obstacle_color": "darkcyan",
+    "obstacle_color": "indianred",
     "collision_point_marker": "*",
     "collision_point_color": "red",
     "init_color": "indianred",
@@ -85,8 +85,8 @@ def visualize_path(path : list, collision_points = None):
     for waypt in path:
         x_pts.append(waypt[0])
         y_pts.append(waypt[1])
-    #ax.scatter(x_pts, y_pts, ls=visualize_config["path_line_style"], color=visualize_config["path_line_color"], lw=visualize_config["path_line_width"], zorder=2)
-    ax.scatter(x_pts, y_pts, marker=visualize_config["path_point_marker"], color=visualize_config["path_line_color"], zorder=2)
+    # ax.scatter(x_pts, y_pts, marker=visualize_config["path_point_marker"], color=visualize_config["path_line_color"], zorder=2)
+    ax.plot(x_pts, y_pts, marker=visualize_config["path_point_marker"], color=visualize_config["path_line_color"], zorder=2)
     if collision_points is not None:
         for colpt in collision_points:
             x_col_pts.append(colpt[0])
---
+++ ws/hw5/MyGDAlgorithm.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// This includes all of the necessary header files in the toolbox
+#include "AMPCore.h"
+
+// Include the correct homework header
+#include "hw/HW5.h"
+
+class MyGDAlgorithm : public amp::GDAlgorithm {
+	public:
+		// Consider defining a class constructor to easily tune parameters, for example: 
+		MyGDAlgorithm(double d_star, double zetta, double Q_star, double eta) :
+			d_star(d_star),
+			zetta(zetta),
+			Q_star(Q_star),
+			eta(eta) {}
+
+		// Override this method to solve a given problem.
+		virtual amp::Path2D plan(const amp::Problem2D& problem) override;
+	private:
+		double d_star, zetta, Q_star, eta;
+		// Add additional member variables here...
+};
+
+class MyPotentialFunction : public amp::PotentialFunction2D {
+    public:
+		// Returns the potential function value (height) for a given 2D point. 
+        virtual double operator()(const Eigen::Vector2d& q) const override {
+            return q[0] * q[0] + q[1] * q[1];
+        }
+};
